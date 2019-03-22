19826f06625dca9609fcb51e4ff8e6f4b62f8a45
Ramakant Singh <ramakant@codeaurora.org>
2019-03-22 18:52:13 +0530

    sf: Allow VDS to use HWC
    -- Preserve VDS layer pixel format based
       on GRALLOC flags.
    -- skip color layer for vds
    
     CRs-Fixed: 2178626
    
    Change-Id: Ie86a28b7743a76c4a46d4b3362f6c504524a3bf9

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -120,6 +120,7 @@
 
 #include <layerproto/LayerProtoParser.h>
 #include "SurfaceFlingerProperties.h"
+#include "gralloc_priv.h"
 
 #ifdef QCOM_UM_FAMILY
 #include "gralloc_priv.h"
@@ -2781,7 +2782,6 @@ void SurfaceFlinger::processDisplayChangesLocked() {
                             ALOGE_IF(status != NO_ERROR, "Unable to query format (%d)", status);
                             auto format = static_cast<ui::PixelFormat>(intFormat);
 
-#ifdef QCOM_UM_FAMILY
                             if (maxVirtualDisplaySize == 0 ||
                                 ((uint64_t)width <= maxVirtualDisplaySize &&
                                 (uint64_t)height <= maxVirtualDisplaySize)) {
@@ -2789,15 +2789,14 @@ void SurfaceFlinger::processDisplayChangesLocked() {
                                 // Replace with native_window_get_consumer_usage ?
                                 status = state.surface->getConsumerUsage(&usage);
                                 ALOGW_IF(status != NO_ERROR, "Unable to query usage (%d)", status);
+                                displayId =
+                                getHwComposer().allocateVirtualDisplay(width, height, &format);
+                                ALOGW_IF(status != NO_ERROR, "Unable to query usage (%d)", status);
                                 if ((status == NO_ERROR) && canAllocateHwcDisplayIdForVDS(usage)) {
                                      displayId =
                                      getHwComposer().allocateVirtualDisplay(width, height, &format);
                                 }
                             }
-#else
-                            displayId =
-                                    getHwComposer().allocateVirtualDisplay(width, height, &format);
-#endif
                         }
 
                         // TODO: Plumb requested format back up to consumer
@@ -3534,14 +3533,12 @@ bool SurfaceFlinger::doComposeSurfaces(const sp<DisplayDevice>& displayDevice,
                 }
                 case Hwc2::IComposerClient::Composition::CLIENT: {
                     renderengine::LayerSettings layerSettings;
-#ifdef QCOM_UM_FAMILY
                     if (displayDevice->isVirtual() &&
                         skipColorLayer(layer->getTypeId())) {
                         // We are not using h/w composer.
                         // Skip color (dim) layer for WFD direct streaming.
                         continue;
                     }
-#endif
                     bool prepared =
                             layer->prepareClientLayer(renderArea, clip, clearRegion,
                                                       supportProtectedContent, layerSettings);
